c6d1eda7ff8b8ffbe7f0f56f37dee962fb2f4c30
jellyfishgh <1129088994@qq.com>
2016-06-18 21:56:19 +0800

    js object create demo in people.js

--- People.js
+++ People.js
@@ -4,12 +4,25 @@ function People(name, location, subjects) {
     this.subjects = subjects;
 }
 
+People.prototype.sayHi = function(){
+    return `${this.name} lives in ${this.location.city}, ${this.location.province} and learns ${this.subjects.join(', ')}.`;
+}
+
 function Animal(name) {
     this.name = name;
     return new Animal("cat");
 }
 
-var people = new People("jellyfish", {
+var p0 = new People("jellyfish", {
     "province": "GuangDong",
     "city": "GuangZhou"
 }, ["Math", "English"]);
+console.log(p0.sayHi());
+
+var p = Object.create(new People());
+console.log(p);
+People.call(p, "jellyfish", {
+    "province": "GuangDong",
+    "city": "GuangZhou"
+}, ["Math", "English"]);
+console.log(p.sayHi());
