9699fb39ef763d8af5d5574ca851d92c35ca894a
Jellyfish <1129088994@qq.com>
2016-06-19 18:24:17 +0800

    rename people.js to constructor.js from win10

--- constructor.js
+++ constructor.js
@@ -6,23 +6,34 @@ function People(name, location, subjects) {
 
 People.prototype.sayHi = function(){
     return `${this.name} lives in ${this.location.city}, ${this.location.province} and learns ${this.subjects.join(', ')}.`;
-}
-
-function Animal(name) {
-    this.name = name;
-    return new Animal("cat");
-}
+};
 
 var p0 = new People("jellyfish", {
     "province": "GuangDong",
     "city": "GuangZhou"
 }, ["Math", "English"]);
 console.log(p0.sayHi());
+console.log(p0.constructor);
 
-var p = Object.create(new People());
-console.log(p);
+// var p = Object.create(new People());
+// console.log(p);
+var p = {};
 People.call(p, "jellyfish", {
     "province": "GuangDong",
     "city": "GuangZhou"
 }, ["Math", "English"]);
-console.log(p.sayHi());
+console.log(People.prototype.sayHi.call(p));
+console.log(p.constructor);
+
+
+function Animal(name) {
+    this.name = name;
+    return `animal: ${name}`;
+}
+
+Animal.prototype.showMe = function(){
+    return `${this.constructor} -> ${this.name}`;
+};
+
+var a0 = new Animal("cat");
+console.log(a0.showMe());
